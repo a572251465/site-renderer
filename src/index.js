eee2d0dc02d857c4c28f241af9ec381578306956
lihaohao <2495041749@qq.com>
2022-11-07 22:38:24 +0800

    feat: 运行é配置文件

--- src/index.js
+++ src/index.js
@@ -23,12 +23,19 @@ const { setNavData } = require('./utils')
 
   // 下载模板
   await downloadTpl(options)
+  console.log('√ 1. template download succeeded')
   // 构建数据 读取md文件 在指定编译目录下重新生成mdx
   makeData()
+  console.log('√ 2. read the md (x) file and generate a new file')
   // 构建json对象的数据
   const arrData = genData()
+  console.log('√ 3. format data successfully')
   // 替换节点
   replaceNode(options, arrData)
+  console.log('√ 4. template keyword replaced successfully')
   // 编译文件
   compileFile()
+  console.log('√ 5. resource compilation succeeded')
+
+  console.log('end of execution')
 })()
